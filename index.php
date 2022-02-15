65194017b48e6f9200c16d0c6b1d53e4921e00d0
Danielzinho <christiandanielgomes@gmail.com>
2022-02-15 16:17:26 -0300

    replacing navbars include

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
     include_once './defaults/header.php';
-    try{
-    include_once './defaults/navbar.php';
-    }catch(Exception $e){
-        print_r($e);
-    }
 ?>
 <title>Home</title>
 
@@ -18,6 +13,55 @@
 ?>
 
 <body>
+    <nav>
+        <div class="nav-bg"></div>
+        <ul>
+            <li><a href="../index.php"><i class="far fa-newspaper"></i></a></li>
+            <li><a href="../index.php">Home</a></li>
+            <li>
+                <a href="">Topics</a>
+                <div class="topic-list">
+                    <?php 
+                        include_once '../includes/dbh.inc.php';
+
+                        $sql = 'SELECT DISTINCT topics.id, topics.name FROM topics INNER JOIN posts ON topics.id = posts.id_topic WHERE deleted = 0';
+
+                        foreach($dbh->query($sql) as $row){ 
+                            echo '<div class="topic-item"><a href="../views/topic.php?id='.$row['id'].'">'.$row["name"].'</a></div>';
+                        };
+
+                    ?>
+                </div>
+            </li>
+            <li>
+                <?php 
+                    if(isset($_SESSION['id'])){
+                        echo '<a href="">'.ucfirst($_SESSION['name']).'</a>';
+                        echo '<div class="dropdown">
+                            <a href="../views/add-post.php">Create post</a>';
+                        if($_SESSION['adm'] === 0){
+                            echo '<a href="../views/profile.php">Profile</a>';
+                        }
+                        if($_SESSION['adm'] === 1){
+                            echo '<a href="../views/accounts.php">Account list</a>';
+                        }
+                        echo '<a href="../includes/logout.inc.php">Log out</a>
+                        </div>';
+                    }else{
+                        echo '<a href="../views/enter.php">Enter</a>';
+                    }
+                ?>
+                </li>
+            <li>
+                <form action="../views/results.php" method="GET">
+                    <input name="search" placeholder="Search..." autocomplete="off">
+                    <button type="button"><i class="fa fa-search"></i></button>
+                </form>
+                <div class="navbar-suggestions"></div>
+            </li>
+        </ul>
+    </nav>
+    <script src="../scripts/searchbar.js"></script>
 
     <div class="offset">
     
